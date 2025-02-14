c72be0d81eda7e7c99d39223a94e53ec2f7b7c13
Shuntaro Kanke <taro.may11@gmail.com>
2025-02-14 23:09:13 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"gin-fleamarket/controllers"
+	"gin-fleamarket/models"
+	"gin-fleamarket/repositories"
+	"gin-fleamarket/services"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	items := []models.Item{
+		{Id: 1, Name: "item1", Price: 1000, Description: "description1", Soldout: false},
+		{Id: 2, Name: "item2", Price: 2000, Description: "description2", Soldout: true},
+		{Id: 3, Name: "item3", Price: 3000, Description: "description3", Soldout: false},
+	}
+
+	itemRepository := repositories.NewItemMemoryRepository(items)
+	itemService := services.NewItemService(itemRepository)
+	itemController := controllers.NewItemController(itemService)
+
+	r := gin.Default()
+	r.GET("/items", itemController.FindAll)
+	r.Run("localhost:8080") // 0.0.0.0:8080 でサーバーを立てます。
+}
---
+++ models/item.go
@@ -0,0 +1,9 @@
+package models
+
+type Item struct {
+	Id          uint
+	Name        string
+	Price       uint
+	Description string
+	Soldout     bool
+}
